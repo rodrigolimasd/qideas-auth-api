165ce068115d50695a713c2f6f401f1d6abdda19
Rodrigo Lima <rodrigolimasd@gmail.com>
2021-03-09 16:41:35 -0300

    initial commit

---
+++ src/main/java/com/rodtech/qideasauthapi/QideasAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.rodtech.qideasauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QideasAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QideasAuthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodtech/qideasauthapi/QideasAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodtech.qideasauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QideasAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
